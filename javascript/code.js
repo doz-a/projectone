05716f520d9dc0e1447c25254bc1e98ab3c21079
Miguel Delos Santos <mendoza.d.santos@gmail.com>
2019-11-21 19:59:22 -0800

    add pseudocode to java

--- javascript/code.js
+++ javascript/code.js
@@ -1,1 +1,9 @@
 console.log('ryan bidgoli');
+
+// document ready 
+
+// ajax function to search for liquor preference 
+// ajax function to search for ingredient preference 
+
+// append 10 drinks to the Drinks List div
+
